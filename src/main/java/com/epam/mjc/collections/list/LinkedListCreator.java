c1003e7634bf6185d8eb89b8dbc53a437a83ae3b
hydroponka <Ceageenk02@gmail.com>
2023-04-25 15:27:31 +0300

    1

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> result = new LinkedList<>();
+        for(Integer number : sourceList){
+            if (number % 2 != 0){
+                result.addFirst(number);
+            }else {
+                result.addLast(number);
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,19 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        ListComparator listComparator = new ListComparator();
+        sourceList.sort(listComparator);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        return funcX(a) - funcX(b);
+    }
+
+    private int funcX(String s){
+        int x = Integer.parseInt(s);
+        return (int) (5*(Math.pow(x,2))+3);
     }
 }
